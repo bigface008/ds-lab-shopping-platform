cfe2d00da8869d90fc126cfd6457d941e0755288
Ice Cola <icecola2018@gmail.com>
2019-07-14 17:00:10 +0800

    修复start-receiver.sh的文件名错误bug

--- script/receiver/start-receiver.sh
+++ script/receiver/start-receiver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sed "s/SERVERID/"$1"/g" < config/application.properties.template > config/application.properties
+sed "s/SERVERID/"$1"/g" < config/application.properties.example > config/application.properties
 nohup java -jar receiver.jar >>receiver.log 2>&1 &
